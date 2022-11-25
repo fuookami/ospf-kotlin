47c9f50713546fd45c87b54934eff776a8a63f8e
fuookami <fuookami@163.com>
2022-11-25 17:42:16 +0800

    feature: impl relative position caculation.

--- ospf-kotlin-utils/src/main/fuookami/ospf/kotlin/utils/math/geometry/Point.kt
+++ ospf-kotlin-utils/src/main/fuookami/ospf/kotlin/utils/math/geometry/Point.kt
@@ -8,14 +8,16 @@ interface Point {}
 data class Point2(
     val x: Flt64,
     val y: Flt64
-) : Point, Plus<Point2, Point2> {
+) : Point, Plus<Point2, Point2>, Minus<Point2, Point2> {
     override fun plus(rhs: Point2) = Point2(x + rhs.x, y + rhs.y)
+    override fun minus(rhs: Point2) = Point2(x - rhs.x, y - rhs.y)
 }
 
 data class Point3(
     val x: Flt64,
     val y: Flt64,
     val z: Flt64
-) : Point, Plus<Point3, Point3> {
+) : Point, Plus<Point3, Point3>, Minus<Point3, Point3> {
     override fun plus(rhs: Point3) = Point3(x + rhs.x, y + rhs.y, z + rhs.z)
+    override fun minus(rhs: Point3) = Point3(x - rhs.x, y - rhs.y, z - rhs.z)
 }
