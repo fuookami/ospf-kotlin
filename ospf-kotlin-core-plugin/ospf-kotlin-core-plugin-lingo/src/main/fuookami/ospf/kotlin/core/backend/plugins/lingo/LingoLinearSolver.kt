ad7de1ab38fd7708c8918fb3bfc62988efa42e4a
fuookami <wfuookami@gmail.com>
2024-12-07 21:23:50 +0800

    chore: add lingo and mosek.

---
+++ ospf-kotlin-core-plugin/ospf-kotlin-core-plugin-lingo/src/main/fuookami/ospf/kotlin/core/backend/plugins/lingo/LingoLinearSolver.kt
@@ -0,0 +1,4 @@
+package fuookami.ospf.kotlin.core.backend.plugins.lingo
+
+class LingoLinearSolver {
+}
---
+++ ospf-kotlin-core-plugin/ospf-kotlin-core-plugin-mosek/src/main/fuookami/ospf/kotlin/core/backend/plugins/mosek/MosekLinearSolver.kt
@@ -0,0 +1,4 @@
+package fuookami.ospf.kotlin.core.backend.plugins.mosek
+
+class MosekLinearSolver {
+}
