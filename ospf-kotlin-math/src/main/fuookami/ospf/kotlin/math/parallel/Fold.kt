33823f8cd8c52e53cd0e63f13c184aec0681854c
fuookami <wfuookami@gmail.com>
2026-04-06 19:35:35 +0800

    perf(parallel): add chunk-based coroutine limiting for parallel fold
    - Add chunkSize parameter (default 100) to limit coroutine count
    - sumOfParallelly: fold each chunk sequentially before combining
    - trySumOfParallelly: preserve short-circuit error semantics within chunks
    - exTrySumOfParallelly: collect errors from all chunks, maintain aggregation semantics

--- ospf-kotlin-math/src/main/fuookami/ospf/kotlin/math/parallel/Fold.kt
+++ ospf-kotlin-math/src/main/fuookami/ospf/kotlin/math/parallel/Fold.kt
@@ -35,13 +35,20 @@ internal fun <T> exResultOf(value: T, errors: List<fuookami.ospf.kotlin.utils.er
 
 suspend inline fun <T, U> Iterable<T>.sumOfParallelly(
     constants: ArithmeticConstants<U>,
+    chunkSize: Int = 100,
     crossinline extractor: SuspendExtractor<U, T>
 ): U where U : Arithmetic<U>, U : Plus<U, U> {
     return coroutineScope {
+        val elements = this@sumOfParallelly.toList()
+        val chunks = elements.chunked(chunkSize)
+
         val promises = ArrayList<Deferred<U>>()
-        for (element in this@sumOfParallelly) {
-            promises.add(async(Dispatchers.Default) { extractor(element) })
+        for (chunk in chunks) {
+            promises.add(async(Dispatchers.Default) {
+                chunk.fold(constants.zero) { acc, element -> acc + extractor(element) }
+            })
         }
+
         var sum = constants.zero
         for (promise in promises) {
             sum = sum + promise.await()
@@ -51,20 +58,36 @@ suspend inline fun <T, U> Iterable<T>.sumOfParallelly(
 }
 
 suspend inline fun <T, reified U> Iterable<T>.sumOfParallelly(
+    chunkSize: Int = 100,
     crossinline extractor: SuspendExtractor<U, T>
 ): U where U : Arithmetic<U>, U : Plus<U, U> {
-    return sumOfParallelly(resolveArithmeticConstants<U>("Fold"), extractor)
+    return sumOfParallelly(resolveArithmeticConstants<U>("Fold"), chunkSize, extractor)
 }
 
 suspend inline fun <T, U> Iterable<T>.trySumOfParallelly(
     constants: ArithmeticConstants<U>,
+    chunkSize: Int = 100,
     crossinline extractor: SuspendTryExtractor<U, T>
 ): Ret<U> where U : Arithmetic<U>, U : Plus<U, U> {
     return coroutineScope {
+        val elements = this@trySumOfParallelly.toList()
+        val chunks = elements.chunked(chunkSize)
+
         val promises = ArrayList<Deferred<Ret<U>>>()
-        for (element in this@trySumOfParallelly) {
-            promises.add(async(Dispatchers.Default) { extractor(element) })
+        for (chunk in chunks) {
+            promises.add(async(Dispatchers.Default) {
+                var sum = constants.zero
+                for (element in chunk) {
+                    when (val ret = extractor(element)) {
+                        is Ok -> sum = sum + ret.value
+                        is Failed -> return@async Failed(ret.error)
+                        is Fatal -> return@async Fatal(ret.errors)
+                    }
+                }
+                Ok(sum)
+            })
         }
+
         var sum = constants.zero
         for (promise in promises) {
             when (val ret = promise.await()) {
@@ -78,34 +101,50 @@ suspend inline fun <T, U> Iterable<T>.trySumOfParallelly(
 }
 
 suspend inline fun <T, reified U> Iterable<T>.trySumOfParallelly(
+    chunkSize: Int = 100,
     crossinline extractor: SuspendTryExtractor<U, T>
 ): Ret<U> where U : Arithmetic<U>, U : Plus<U, U> {
-    return trySumOfParallelly(resolveArithmeticConstants<U>("Fold"), extractor)
+    return trySumOfParallelly(resolveArithmeticConstants<U>("Fold"), chunkSize, extractor)
 }
 
 suspend inline fun <T, U> Iterable<T>.exTrySumOfParallelly(
     constants: ArithmeticConstants<U>,
+    chunkSize: Int = 100,
     crossinline extractor: SuspendTryExtractor<U, T>
 ): fuookami.ospf.kotlin.utils.functional.ExRet<U> where U : Arithmetic<U>, U : Plus<U, U> {
     return coroutineScope {
-        val promises = ArrayList<Deferred<Ret<U>>>()
-        for (element in this@exTrySumOfParallelly) {
-            promises.add(async(Dispatchers.Default) { extractor(element) })
+        val elements = this@exTrySumOfParallelly.toList()
+        val chunks = elements.chunked(chunkSize)
+
+        val promises = ArrayList<Deferred<Pair<U, List<fuookami.ospf.kotlin.utils.error.Error>>>>()
+        for (chunk in chunks) {
+            promises.add(async(Dispatchers.Default) {
+                val errors = ArrayList<fuookami.ospf.kotlin.utils.error.Error>()
+                var sum = constants.zero
+                for (element in chunk) {
+                    when (val ret = extractor(element)) {
+                        is Ok -> sum = sum + ret.value
+                        is Failed, is Fatal -> errors.appendFrom(ret)
+                    }
+                }
+                Pair(sum, errors)
+            })
         }
+
         val errors = ArrayList<fuookami.ospf.kotlin.utils.error.Error>()
         var sum = constants.zero
         for (promise in promises) {
-            when (val ret = promise.await()) {
-                is Ok -> sum = sum + ret.value
-                is Failed, is Fatal -> errors.appendFrom(ret)
-            }
+            val (chunkSum, chunkErrors) = promise.await()
+            sum = sum + chunkSum
+            errors.addAll(chunkErrors)
         }
         exResultOf(sum, errors)
     }
 }
 
 suspend inline fun <T, reified U> Iterable<T>.exTrySumOfParallelly(
+    chunkSize: Int = 100,
     crossinline extractor: SuspendTryExtractor<U, T>
 ): fuookami.ospf.kotlin.utils.functional.ExRet<U> where U : Arithmetic<U>, U : Plus<U, U> {
-    return exTrySumOfParallelly(resolveArithmeticConstants<U>("Fold"), extractor)
+    return exTrySumOfParallelly(resolveArithmeticConstants<U>("Fold"), chunkSize, extractor)
 }
